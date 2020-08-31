245196f1970eae54aa04e15aad6bbe8c74383e5f
mhd3v <mamir3012@gmail.com>
2020-08-31 15:17:04 +0500

    Clean up styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'local-search-results-checker';
+
+  searchEngine = 'google';
+
+  isFocused(el) {
+    return document.activeElement === el || el.value;
+  }
+
+  setFocus(el) {
+    el.focus();
+  }
+
+  onBlur() {
+  }
+
 }
