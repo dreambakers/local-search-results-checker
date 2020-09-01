351b680b1c5d1b7f3e71bac3edf246da9d5ad424
mhd3v <mamir3012@gmail.com>
2020-09-01 12:06:54 +0500

    Fix responsiveness on results page

--- src/app/local-search-results/local-search-results.component.ts
+++ src/app/local-search-results/local-search-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-local-search-results',
@@ -9,11 +9,28 @@ export class LocalSearchResultsComponent implements OnInit {
 
   pages = [
     1,2,3,4,5,6,7,8,9,10
-  ]
+  ];
+
+  innerWidth;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+  }
+
+  get cellWidth() {
+    if (this.innerWidth <= 650) {
+      console.log('yes')
+      return 95;
+    }
+
+    return 180;
   }
 
 }
